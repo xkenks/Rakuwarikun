d7c647d04b6826c7961324c41ac77d08fa617fa6
田川健介 <kensuke@tagawakensukenoMacBook-Pro.local>
2025-03-09 15:40:18 +0900

    ファビコンとPWA用アイコンを追加

--- src/pages/document.js
+++ src/pages/document.js
@@ -1,18 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Document from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head>
-          {/* ここにメタタグやフォントのリンクを追加 */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#004085" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
